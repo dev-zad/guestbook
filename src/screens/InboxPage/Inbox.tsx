46efe62b320e783fd67ef846c554a7eda73a87dd
zadrach caunca <zadrachcaunca777@gmail.com>
2024-05-28 02:11:30 +0800

    chore: Remove handleLogout function from InboxPage component

--- src/screens/InboxPage/Inbox.tsx
+++ src/screens/InboxPage/Inbox.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import ProtectedRoute from "../ProtectedRoute";
 // import MessageInbox from "./content/MessageInbox";
 import DemoPage from "./content/connects/page";
 
 export function InboxPage() {
 
-  const handleLogout = () => {
-    // Remove the user's data from local storage
-    localStorage.removeItem('user');
-
-    // Redirect to the login page
-    window.location.href = '/';
-  };
   return (
     <ProtectedRoute>
       <div className="flex flex-col py-20">
